c016e96e3c9c42878d6c98b35a2e4e7ed462ffa4
catalinaep <catalina.eva.padilla@gmail.com>
2026-04-28 20:08:38 -0600

    modified adjlist class

--- src/graphs.py
+++ src/graphs.py
@@ -1,8 +1,8 @@
 class AdjMatrix:
     def __init__(self, size):
         self.adj_matrix = [[0] * size for _ in range(size)]
-        self.size = size #num of vertices
-        self.vertex_data = [''] * size #names of vertices
+        self.size = size # number of vertices
+        self.vertex_data = [''] * size # names of vertices
         
     def add_edge(self, u, v, weight):
         if 0 <= u < self.size and 0 <= v < self.size:
@@ -14,17 +14,17 @@ class AdjMatrix:
             self.vertex_data[vertex] = data
 
 class AdjList:
-    def __init__(self, size):
+    def __init__(self):
         self.adj_list = {}
     
     def add_vertex(self, v):
         if v not in self.adj_list:
             self.adj_list[v] =[]
 
-    def add_edge(self, u, v):
+    def add_edge(self, u, v, weight=1):
         self.add_vertex(u)
         self.add_vertex(v)
 
-        #graph expected to be undirected
-        self.adj_list[u].append(v)
-        self.adj_list[v].append(u)
+        # graph expected to be undirected
+        self.adj_list[u].append((v, weight))
+        self.adj_list[v].append((u, weight))
