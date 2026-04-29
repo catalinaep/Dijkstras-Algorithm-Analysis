3421e6a68e2308eea354d0aa3850484e491599a6
catalinaep <catalina.eva.padilla@gmail.com>
2026-04-28 20:02:56 -0600

    changed name

---
+++ src/graphs.py
@@ -0,0 +1,30 @@
+class AdjMatrix:
+    def __init__(self, size):
+        self.adj_matrix = [[0] * size for _ in range(size)]
+        self.size = size #num of vertices
+        self.vertex_data = [''] * size #names of vertices
+        
+    def add_edge(self, u, v, weight):
+        if 0 <= u < self.size and 0 <= v < self.size:
+            #graph is expected to be undirected
+            self.adj_matrix[u][v] = weight
+            self.adj_matrix[v][u] = weight
+    def add_vertex_data(self, vertex, data):
+        if 0 <= vertex < self.size:
+            self.vertex_data[vertex] = data
+
+class AdjList:
+    def __init__(self, size):
+        self.adj_list = {}
+    
+    def add_vertex(self, v):
+        if n not in self.adj_list:
+            self.adj_list[v] =[]
+
+    def add_edge(self, u, v):
+        self.add_vertex(u)
+        self.add_vertex(v)
+
+        #graph expected to be undirected
+        self.adj_list[u].append(v)
+        self.adj_list[v].append(u)
