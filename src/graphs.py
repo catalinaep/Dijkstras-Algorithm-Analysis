cd83bcd1d6713543b6faf4152492c49582b28c9d
pybiscuit <chemistryrob@gmail.com>
2026-04-30 21:39:21 -0600

    Updated AdjList to implement min-heap priority queue dijkstra algorithm. Added notes. Updated vertex and weight loading. Added all test graphs to dict object GRAPHS

--- src/graphs.py
+++ src/graphs.py
@@ -1,3 +1,49 @@
+"""
+ * Dijkstra Implementation Analysis (Project 2)
+ * 
+ *   Course: CS361
+ *  Section: 001
+ *  Authors:
+ *     Catalina Padilla
+ *     Robert Vanderburg
+ *  Project: Dijkstra Impelemntation Analysis
+ * Filename: graphs.py
+ * 
+ * Description:
+ * Implements Dijkstra's algorithm using 3 different data structures: an array, a matrix, and a min-
+ * heap priority queue. Provides an analysis on timing and memory usage across the different
+ * implementations.
+"""
+
+import heapq
+import time
+import sys
+
+GRAPHS = {
+        'SG1': {
+            'vertices': 'ABCDEF',
+            'weights': [(0,1,4), (0,2,2), (1,3,5), (2,3,1), (3,4,3), (4,5,2)]
+        },
+        'SG2': {
+            'vertices': '1234567',
+            'weights': [(0,1,3), (0,2,6), (1,3,2), (2,4,4), (3,5,6), (4,6,1), (1,4,5)]
+        },
+        'DG1': {
+            'vertices': 'ABCDE',
+            'weights': [
+                (0,1,2), (0,2,5), (0,3,1), (0,4,4), (1,2,3), (1,3,2), (1,4,6), 
+                (2,3,3), (2,4,1), (3,4,2)
+            ]
+        },
+        'DG2': {
+            'vertices': '123456',
+            'weights': [
+                (0,1,3), (0,4,5), (1,2,1), (2,3,3), (3,4,2), (4,5,1), (0,2,2), (1,3,2), 
+                (1,4,4), (2,4,6), (0,3,6), (0,5,4), (1,5,7), (2,5,5), (3,5,4)
+            ]
+        }
+    }
+
 class AdjMatrix:
     def __init__(self, size):
         self.size = size # number of vertices
@@ -49,13 +95,6 @@ class AdjMatrix:
                         dist[v] = relaxation
         
         return dist 
-
-             
-
-
-
-
-
 
 class AdjList:
     def __init__(self, size):
@@ -111,26 +150,64 @@ class AdjList:
         return dist
     
     def dijkstra_priority(self, source):
-        #TODO implement the priority version
-        pass
+        source_vertex = self.vertex_data.index(source)
+        dist = [float('inf')] * self.size
+        dist[source_vertex] = 0
 
-#sparse graph 1 from instructions
-graph = AdjList(6)
-graph.add_vertex_data(0, 'A')
-graph.add_vertex_data(1, 'B')
-graph.add_vertex_data(2, 'C')
-graph.add_vertex_data(3, 'D')
-graph.add_vertex_data(4, 'E')
-graph.add_vertex_data(5, 'F')
+        prev = [None] * self.size
 
-graph.add_edge(0,1,4)
-graph.add_edge(0,2,2)
-graph.add_edge(1,3,5)
-graph.add_edge(2,3,1)
-graph.add_edge(3,4,3)
-graph.add_edge(4,5,2)
+        pq = [(0, source_vertex)]
 
-print("\nDijkstra's Algorithm starting from vertex D:")
-distances = graph.dijkstra_linear('D')
-for i , d in enumerate(distances):
-    print(f"Distance from D to {graph.vertex_data[i]}: {d}")
+        visited = set()
+
+        while pq:
+            current_dist, u = heapq.heappop(pq)
+
+            if u in visited:
+                continue
+
+            visited.add(u)
+
+            if current_dist > dist[u]:
+                continue
+
+            for v, weight in self.adj_list[u]:
+                if v not in visited:
+                    relax = dist[u] + weight
+
+                    if relax < dist[v]:
+                        dist[v] = relax
+                        prev[v] = u
+                        heapq.heappush(pq, (relax, v))
+        return dist
+
+def load_vertex_weights(graph_key, dijkstra):
+    """
+    Load the given graph data using graph_key from GRAPH into the Dijkstra algorithm class.
+
+    :param graph_key: String value key representing a graph in GRAPH.
+    :param dijkstra: AdjMatrix or AdjList class representing algorithm implmentation.
+    """
+    for idx, n in enumerate(GRAPHS[graph_key]['vertices']):
+        dijkstra.add_vertex_data(idx, n)
+
+    for idx, n in enumerate(GRAPHS[graph_key]['weights']):
+        dijkstra.add_edge(n[0], n[1], n[2])
+
+
+if __name__ == "__main__":
+    
+    #sparse graph 1 from instructions
+    graph = AdjList(6)
+
+    load_vertex_weights('SG1', graph)
+
+    print("\nDijkstra's Algorithm starting from vertex D:")
+    distances = graph.dijkstra_linear('D')
+    priority_dist = graph.dijkstra_priority('D')
+    for i , d in enumerate(distances):
+        print(f"Distance from D to {graph.vertex_data[i]}: {d}")
+
+    print("\nDijkstra's Priority starting from vertex D:")
+    for i , d in enumerate(priority_dist):
+        print(f"Distance from D to {graph.vertex_data[i]}: {d}")
